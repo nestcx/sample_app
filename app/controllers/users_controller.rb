4af761ebff793b8d52624eeeec7ba81aef0bc149
Marcus <marcusvalentino2013@gmail.com>
2016-12-08 18:10:39 +1100

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated == true
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -56,14 +57,6 @@ class UsersController < ApplicationController
 	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	  end
 
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
     # Confirms the correct user.
     def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  has_many :microposts, dependent: :destroy
+
 	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email 
 
@@ -69,6 +71,10 @@ class User < ApplicationRecord
 
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
